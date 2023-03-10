04bd6ac5ab8c62f42bc763356ecd491e79571885
Thomas White <twhite@yext.com>
2023-03-10 14:02:02 -0500

    fix

--- functions/api/status/endpoint.ts
+++ functions/api/status/endpoint.ts
@@ -2,7 +2,7 @@ export async function main(argumentJson) {
 	console.log(argumentJson)
 
 	return {
-		"body": `healthy and running, env var: ${ENV_VAR_KEY}`,
+		"body": `healthy and running, env var`,
 		"statusCode": 200,
 		"headers": {
 			// "Cache-control": "private",
