1ef2bac67d2faea0b0f2ec51e2c78b5c10c28c36
dlsgusrn7577 <dlee@yext.com>
2023-03-17 13:02:23 -0400

    Update mod.ts

--- complex-functions/api/{{sessionId}}/games/mod.ts
+++ complex-functions/api/{{sessionId}}/games/mod.ts
@@ -7,19 +7,15 @@ export interface PluginResponse {
   body: string,
   statusCode: number,
   flag?: string,
-  headers?: Record<string, string>
+  headers?: Record<string, string>,
+  site?: Record<string, string>
 }
 
 export async function main(argumentJson: PluginInput): Promise<PluginResponse> {
-	console.log('dang');
-	console.log(argumentJson.pathParams.sessionId);
-	console.log(argumentJson);
-	console.log('pondg');
-
 	return {
 		body: JSON.stringify({
-			count: argumentJson.pathParams.sessionId,
-			count2: 1,
+			sessionId: argumentJson.pathParams.sessionId,
+			businessName: argumentJson.site.businessName,
 		}),
 		statusCode: 200,
 		headers: {"Content-Type": "application/json"}
