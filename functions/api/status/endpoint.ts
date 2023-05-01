3cb852e3616faf5045b98a60f7de1e4b0acced69
Thomas White <twhite@yext.com>
2023-05-01 16:02:31 -0400

    add setTimeout

--- functions/api/status/endpoint.ts
+++ functions/api/status/endpoint.ts
@@ -1,12 +1,14 @@
 export async function main(argumentJson) {
 	console.log(argumentJson)
 
-	return {
-		"body": `healthy and running, env var`,
-		"statusCode": 200,
-		"headers": {
-			// "Cache-control": "private",
-			"X-Yext-Test": "Example header",
-		},
-	  };
+	setTimeout(()=>{
+		return {
+			"body": `healthy and running, env var`,
+			"statusCode": 200,
+			"headers": {
+				// "Cache-control": "private",
+				"X-Yext-Test": "Example header",
+			},
+		  };
+	}, 2000)
 }
